58d780b26195cdbb3ae5e49a6be801c3045acfd6
rahulremanan <rahul@remanan.net>
2018-02-17 17:10:53 -0500

    Added front end and back end examples

---
+++ front_end/config.py
@@ -0,0 +1,4 @@
+PROJECT_ID = 'HIMA'
+MAX_CONTENT_LENGTH = 8 * 1024 * 1024
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+PREDICTION_SERVICE_URL = 'http://192.168.3.117:8080'
---
+++ front_end/main.py
@@ -0,0 +1,87 @@
+import config
+import logging
+from flask import current_app, Flask, render_template, request, flash, \
+send_file, send_from_directory
+import urllib
+import base64
+import json
+from werkzeug import secure_filename
+import os
+import requests
+from PIL import Image
+from io import BytesIO
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+import random
+import string
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = '/home/rahul/Jomiraki/upload/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+SERVER_URL = 'http://192.168.2.2:8080'
+APP_URL = 'http://192.168.2.2:5000'
+
+app.config.from_object(config)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SERVER_URL'] = SERVER_URL
+app.config['APP_URL'] = APP_URL
+
+logging.basicConfig(level=logging.INFO)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def fetch_predictions(img_stream=None):
+    img_stream=img_stream
+    ENCODING = 'utf-8'
+    server_url = app.config['SERVER_URL'] + '/async/'
+    base64_string = base64.b64encode(img_stream)
+    r = requests.post(server_url, files={'file': base64_string})
+    print(r.text)
+    return r
+
+def fetch_predictions_filename(filename=None):
+    filename=filename
+    server_url = app.config['SERVER_URL'] + '/async/upload/'
+    file = (os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    files = {'file': open(file, 'rb')}
+    r = requests.post(server_url, files=files)
+    print(r.text)
+    return r
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        img = request.files.get('file')
+        N = 64
+        filename = (''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N)) + '.png')
+        print ('Received file: ... ' + secure_filename(img.filename))
+        print (filename)
+        img_stream = img.read()
+        img_data= Image.open(BytesIO(img_stream))
+        img_data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        predictions = fetch_predictions_filename(filename=filename)
+        return render_template('view.html', \
+                               image_url=app.config['APP_URL']+ '/uploads/' \
+                               + filename,\
+                               predictions=predictions)
+    else:
+        flash("Image file not supported ...")
+        return render_template('form.html')
+
+@app.route('/uploads/<filename>')
+def fetch_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.error('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+
+if __name__=="__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
