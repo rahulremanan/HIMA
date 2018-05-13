9084366518a8bc1d8280f4daf02a7b48708689f2
rahulremanan <rahul@remanan.net>
2018-05-13 00:23:14 -0400

    Updated license details to edited Mask_RCNN configuration file

--- examples/Notebooks/04_Kaggle_DSB_2018/src/Mask_RCNN/config.py
+++ examples/Notebooks/04_Kaggle_DSB_2018/src/Mask_RCNN/config.py
@@ -2,9 +2,9 @@
 Mask R-CNN
 Base Configurations class.
 
-Copyright (c) 2017 Matterport, Inc.
+Copyright (c) 2018 Ekaveda, Inc.
 Licensed under the MIT License (see LICENSE for details)
-Written by Waleed Abdulla
+Written by Rahul Remanan
 """
 
 import math
